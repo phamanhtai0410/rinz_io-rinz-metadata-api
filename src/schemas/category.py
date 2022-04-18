af26738d6a98678cb05f9489756631e401b279a4
khoabd <khoabd98@gmail.com>
2022-04-18 10:12:58 +0700

    add default_banner

--- src/schemas/category.py
+++ src/schemas/category.py
@@ -13,6 +13,8 @@ from random import randint
 
 from src.enums.obj import ObjType
 
+default_banner = 'https://s3-alpha-sig.figma.com/img/402b/7c6c/53824e64a9ae4cbe850f69d339e1d379?Expires=1651449600&Signature=gv5HshfnCMLIeP3BD8d0kMDzBd36MdomUpS2pGNVc8JuvLl9aWdEJsv0WXyIj3gceHArsJsD0j0NEswmh9XiIBxm4~nkPI5cfLNYjGWNu1wuUtsbXFA9mgL7dZMo1NAHC3tzo4OFnOdnX4pNoDp1AT6z7~xDD3N5eJJxFy40fa8iY4LydQis38984mDRs~2oQNo07Fl1xsFgKE25ba0~fzvH3YBxDPcvIvA-H-6CnOYoh8iA9k1tEeMF9Hpz~jnWaXZdanGoTaFNtXVVFep1FiWjTytMxlXYJlwK9TfxdQ9ynXhl~hLXd6K8Sz8xHBNdZ~2JGDTaJVY0WV6kHff7Uw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+
 
 class UserView(Schema):
     class Meta:
@@ -40,7 +42,9 @@ class BlockView(Schema):
         unknown = EXCLUDE
 
     _id = ObjectIdField(required=True)
-    banner = fields.Str(missing='https://s3-alpha-sig.figma.com/img/402b/7c6c/53824e64a9ae4cbe850f69d339e1d379?Expires=1651449600&Signature=gv5HshfnCMLIeP3BD8d0kMDzBd36MdomUpS2pGNVc8JuvLl9aWdEJsv0WXyIj3gceHArsJsD0j0NEswmh9XiIBxm4~nkPI5cfLNYjGWNu1wuUtsbXFA9mgL7dZMo1NAHC3tzo4OFnOdnX4pNoDp1AT6z7~xDD3N5eJJxFy40fa8iY4LydQis38984mDRs~2oQNo07Fl1xsFgKE25ba0~fzvH3YBxDPcvIvA-H-6CnOYoh8iA9k1tEeMF9Hpz~jnWaXZdanGoTaFNtXVVFep1FiWjTytMxlXYJlwK9TfxdQ9ynXhl~hLXd6K8Sz8xHBNdZ~2JGDTaJVY0WV6kHff7Uw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA', allow_none=True)
+    banner = fields.Str(
+        missing=default_banner,
+        allow_none=True)
     title = fields.Str(missing='')
     description = fields.Str(missing='')
     public_address = fields.Str(required=True)
@@ -52,9 +56,10 @@ class BlockView(Schema):
     def load_user(self, in_data, **kwargs):
         in_data['user'] = get_user_by_id(in_data.get('author_id', ''))
         if 'banners' in in_data and in_data['banners'] and len(in_data['banners']) > 0:
-            in_data['banner'] = get(in_data, 'banners[0].url')  # in_data['banner'][0]['url']
+            in_data['banner'] = get(in_data, 'banners[0].url', default=default_banner)  # in_data['banner'][0]['url']
         else:
-            in_data['banner'] = 'https://s3-alpha-sig.figma.com/img/402b/7c6c/53824e64a9ae4cbe850f69d339e1d379?Expires=1651449600&Signature=gv5HshfnCMLIeP3BD8d0kMDzBd36MdomUpS2pGNVc8JuvLl9aWdEJsv0WXyIj3gceHArsJsD0j0NEswmh9XiIBxm4~nkPI5cfLNYjGWNu1wuUtsbXFA9mgL7dZMo1NAHC3tzo4OFnOdnX4pNoDp1AT6z7~xDD3N5eJJxFy40fa8iY4LydQis38984mDRs~2oQNo07Fl1xsFgKE25ba0~fzvH3YBxDPcvIvA-H-6CnOYoh8iA9k1tEeMF9Hpz~jnWaXZdanGoTaFNtXVVFep1FiWjTytMxlXYJlwK9TfxdQ9ynXhl~hLXd6K8Sz8xHBNdZ~2JGDTaJVY0WV6kHff7Uw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+            in_data[
+                'banner'] = default_banner
         return in_data
 
 
